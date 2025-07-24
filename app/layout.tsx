593b1f4a43d7fdadc99164de3d4df238abecf58d
Saharsh Yeruva <yeruva@saharsh.xyz>
2025-07-24 12:40:54 -0400

    feat: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { NuqsAdapter } from "nuqs/adapters/next/app"
 import { Toaster } from "@/components/ui/sonner"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${inter.className} p-5 antialiased`}>
         <NuqsAdapter>{children}</NuqsAdapter>
         <Toaster richColors expand={false} position="bottom-center" />
+        <Analytics />
       </body>
     </html>
   )
